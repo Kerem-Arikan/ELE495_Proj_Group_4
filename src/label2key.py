8974912d095a75b2ea3debefe8cfebe3752d8580
mcanoz <31281045+mcanoz@users.noreply.github.com>
2021-02-06 16:53:15 +0300

    label2keys changed as class

--- src/label2key.py
+++ src/label2key.py
@@ -1,22 +1,35 @@
-def label2key():
-    keys = open("keymap.txt", "r")
-    gestures = open("../saved_model/labelmap.txt","r")
-    mydic={}
+class label2key(object):
+	"""docstring for label2key"""
+	def __init__(self, keys_path="keymap.txt", gestures_path="../frozen_graph/labelmap.txt"):
+		super(label2key, self).__init__()
+		label2key.__data={}
+		try:
+			keys = open(keys_path, "r")
+			gestures = open(gestures_path,"r")
+			while True:
+				dumbkey = keys.readline()
+				dumbgesture = gestures.readline()
+				# End of file check
+				if dumbkey == '':
+					break
+				if dumbgesture == '':
+					break
+				# Check if there is new line symbol.
+				if dumbkey[-1]=="\n":
+					dumbkey = dumbkey[:-1]
+				if dumbgesture[-1]=='\n':
+					dumbgesture = dumbgesture[:-1]
+				
+				label2key.__data[dumbgesture]=dumbkey
+			keys.close()
+			gestures.close()	
+		except IOError:
+			print("IOError")
+			print("gestures_path="+gestures_path)
+			print("keys_path="+keys_path)
 
-    while True:
-        dumbkey = keys.readline()
-        dumbgesture = gestures.readline()
-        if dumbkey == '':
-            break
-        if dumbgesture == '':
-            break
-        if dumbkey[-1]=="\n":
-            dumbkey = dumbkey[:-1]
-        if dumbgesture[-1]=='\n':
-            dumbgesture = dumbgesture[:-1]
-        mydic[dumbkey]=dumbgesture
+	def label2key(self,LABEL):
+		GESTURE=self.__data[LABEL]
+		return GESTURE
 
-    keys.close()
-    gestures.close()
-
-    return mydic
+		
