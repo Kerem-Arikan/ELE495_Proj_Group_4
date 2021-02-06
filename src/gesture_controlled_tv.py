017e6ce1cabe65eb6eca414930bf013cb5ffe03f
kerem-arikan <kerembio@gmail.com>
2021-02-06 15:20:05 +0300

    Changed graph directory.

--- src/gesture_controlled_tv.py
+++ src/gesture_controlled_tv.py
@@ -1,5 +1,19 @@
 import os
 import time
-import tensorflow as tflite
+from importlib import util
+from argparse import ArgumentParser as argparser
 
-print("Kerem")
+GRAPH_DIR = '../frozen_graph'
+
+argument_parser = argparser()
+argument_parser.add_argument('--graphname', help="Name of the graph file.", default='detect.tflite')
+argument_parser.add_argument('--webcamres', help="Requested resolution of the webcam.", default='1280x720')
+argument_parser.add_argument('--score_threshold', help="Minimum score to confirm the detection as legitimate.", default=0.5)
+
+arguments = argument_parser.parse_args()
+
+GRAPH_PATH = os.path.join(GRAPH_DIR, arguments.graphname)
+
+tflite_runtime_spec = util.find_spec('tflite_runtime')
+
+print(GRAPH_PATH)
