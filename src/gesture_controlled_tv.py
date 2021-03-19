eee562e4790d91207633d50d7fb6db43153e1a89
kerem-arıkan <kerembio@gmail.com>
2021-03-19 12:11:57 +0300

    Added corner cases

--- src/gesture_controlled_tv.py
+++ src/gesture_controlled_tv.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import argparse
 import cv2
 import numpy as np
@@ -56,9 +57,13 @@ parser.add_argument('--allow_cam_display', help='Display the captured image or n
 
 parser.add_argument('--tvname', help="Name of the tv name.", default="kerem-tv") # kerem-tv Samsung_TV starbox
 
-parser.add_argument('--response_rate', help="How quick should the detection be?", default=10)
+parser.add_argument('--response_rate', help="How quick should the detection be?", default=5)
+
+parser.add_argument('--count2send_lim', help="back to back numbers", default=4)
 
 args = parser.parse_args()
+
+send_lim = int(args.count2send_lim)
 
 MODEL_NAME = args.modeldir
 GRAPH_NAME = args.graph
@@ -122,7 +127,8 @@ freq = cv2.getTickFrequency()
 
 videostream = video_capture(resolution=(imW,imH),framerate=30).start()
 time.sleep(1)
-queue=[];count2send=0
+queue=[]
+count2send=0
 prev_obj_name = 0
 counter = 0
 while True:
@@ -149,46 +155,52 @@ while True:
 
     for i in range(len(scores)):
         if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
-            #'''
+            '''
             ymin = int(max(1,(boxes[i][0] * imH)))
             xmin = int(max(1,(boxes[i][1] * imW)))
             ymax = int(min(imH,(boxes[i][2] * imH)))
             xmax = int(min(imW,(boxes[i][3] * imW)))
             cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-            #'''
+            '''
             object_name = labels[int(classes[i])] 
-            #'''
+            '''
             label = '%s: %d%%' % (object_name, int(scores[i]*100)) 
             labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) 
             label_ymin = max(ymin, labelSize[1] + 10) 
             cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
             cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-            #'''
+            '''
             key_name = label_key_map[object_name]
             command_string = "irsend SEND_ONCE" + " " + args.tvname + " " + key_name
             if(prev_obj_name == object_name):
                 counter += 1
                 if(counter==counter_limit):
-                    print(command_string,"only detected not sended")
-                    queue.append(command_string) #os.system(command_string)
+                    print(command_string,"only detected not sent")
+                    if(object_name in ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+                        queue.append(command_string)
+                    else:
+                        os.system(command_string)
                     counter = 0
             else:
                 counter = 0
             prev_obj_name = object_name
-    if(len(queue)>=2) or count2send>=4:
+    if(len(queue)>=2) or count2send>=send_lim:
         print("sending", len(queue),"command(s)")
+        print("Queue: ", queue)
         for s in queue:
-            os.system(s);print(s,"SENDING")
+            os.system(s)
+            time.sleep(0.5)
+            print(s,"SENDING")
         queue=[]
         count2send=0
         
     if(len(queue)>0):
                 count2send+=1;print("line coun2send is:",count2send)
-    #'''
+    '''
     cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
     cv2.imshow('Object detector', frame)
-    #'''
+    '''
     t2 = cv2.getTickCount()
     time1 = (t2-t1)/freq
     frame_rate_calc= 1/time1 #;print(frame_rate_calc)
